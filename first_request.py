df2d8a8812612a5f4ea09ac31317ff2ec52d564d
Kenneth <26774728+kenneth-jiang@users.noreply.github.com>
2018-03-18 04:12:43 -0400

    Added functions and functionality.

--- first_request.py
+++ first_request.py
@@ -1,8 +1,27 @@
+import pyfiglet
+import termcolor
 import requests
+import random
 
-url = "https://icanhazdadjoke.com"
-response = requests.get(url, headers={"Accept": "application/json"})
+def start():
+	print(termcolor.colored(pyfiglet.figlet_format("Dad Jokes 3000"), color="red"))
+	user_input = input("What would you like to search for? ")
+	request(user_input)
 
-data = response.json()
-print(data["joke"])
-print(f"status: {data['status']}")
+def request(user_input):
+	url = "https://icanhazdadjoke.com/search"
+	response = requests.get(
+		url, 
+		headers={"Accept": "application/json"},
+		params={"term": user_input}
+	).json()
+	api_response(response)
+
+def api_response(response):
+	if response["total_jokes"] > 1:
+		print(f"There are {len(response['results'])} jokes with {response['search_term']}, here's a funny one!")
+		print(random.choice(response['results'])['joke'])
+	else:
+		print(f"Sorry, there are no jokes with {response['search_term']}.. =(")
+
+start()
